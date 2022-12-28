baaaffa7a9927fd0dad355e8c3cc8cc67df7a3c4
Will Robinson <william.ellis.robinson@gmail.com>
2022-12-28 20:54:57 +0000

    more listn updates

--- src/modules/Redux/apis/lists/index.ts
+++ src/modules/Redux/apis/lists/index.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+import {
+    BASE_URL,
+    LISTS_URL,
+} from "../../../../constants/index";
+
 export const getList = async (id: string) => {
+    const url = BASE_URL + LISTS_URL;
     try {
-        const response = await axios.get(`lists/${id}`);
+        const response = await axios.get(url + id);
         return response.data;
     } catch (error) {
         console.error(error);
