a640ee884b68e2cb179ae5c35818350f5a7e8f74
Will Robinson <william.ellis.robinson@gmail.com>
2022-12-28 17:28:50 +0000

    lil nvim commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from "react-redux";
+import { fetchListRequest } from "./modules/Redux/actions/lists/actions";
+
 import './App.css';
 
 export const App = () => {
+    const reduxDispatch = useDispatch();
+    const list = useSelector(
+        state => state.list,
+    );
+
+    useEffect(() => {
+        reduxDispatch(fetchListRequest({ id: 1 }))
+    }, [reduxDispatch]);
+
     return (
-        <h1>redux-saga</h1>
+        <>
+            <h1>redux-saga</h1>
+            {list.map((item) => {
+                return <h2>{item.value}</h2>
+            })}
+        </>
     );
 }
 
