a326cfe6ed04f03a09b02f35d4116bc7b552b6dd
Will Robinson <william.ellis.robinson@gmail.com>
2022-12-30 17:47:26 +0000

    reflecting server changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import { AppState } from "./modules/Redux/reducers/rootReducer";
 import {
     fetchListRequest,
+    //completeItemRequest,
     //deleteItemRequest,
+    //addItemRequest,
 } from "./modules/Redux/actions/lists/actions";
 
 import "./App.css";
@@ -15,31 +17,51 @@ export const App = () => {
     const list = useSelector(
         (state: AppState) => state.lists.list,
     );
+    
+    const [newItem, setNewItem] = useState("");
 
     useEffect(() => {
         reduxDispatch(fetchListRequest());
     }, [reduxDispatch]);
 
-    const completeAction = (index) => {
+    const completeItem = (index: number) => {
         console.info("complete");
+        console.info(index);
         //deleteItemRequest(index);
     };
 
-    const deleteAction = (index) => {
+    const deleteItem = (index: number) => {
         console.info("delete");
+        console.info(index);
         //patchItemRequest(index, value);
     };
 
+    const addItem = () => {
+        console.info("add item");
+        //postItemRequest(value);
+    };
+  
     return (
         <>
             <h1>redux-saga</h1>
             {list.map((item, index) => {
                 return <div key={index}>
                     <h2>{item.value}</h2>
-                    <button onClick={() => completeAction(index)}>complete</button>
-                    <button onClick={() => deleteAction(index)}>delete</button>
+                    <button onClick={() => completeItem(index)}>complete</button>
+                    <button onClick={() => deleteItem(index)}>delete</button>
                 </div>
             })}
+            <form onSubmit={addItem}>
+                <label>
+                    Add Item:
+                <input
+                    type="text"
+                    value={newItem}
+                    onChange={e => setNewItem(e.target.value)}
+                />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </>
     );
 }
