00077a9db8137a88a88a0f5143610d4cc368bb00
Will Robinson <william.ellis.robinson@gmail.com>
2023-01-09 19:30:57 +0000

    added first saga test

---
+++ src/modules/Redux/sagas/listsSaga/__tests__/index.test.ts
@@ -0,0 +1,63 @@
+import { call, put, take } from "redux-saga/effects";
+import { expectSaga } from "redux-saga-test-plan";
+
+import { fetchListSaga } from "../index";
+import {
+    fetchListSuccess,
+    fetchListFailure,
+} from "../../../actions/lists/actions";
+import { FETCH_LIST_REQUEST } from "../../../actions/lists/actionTypes";
+import { getList } from "../../../apis/lists";
+
+const mockData = [
+    {
+        "id": 1,
+        "value": "take out the rubbish",
+        "completed": true, 
+    },
+    {
+        "id": 2,
+        "value": "take out the garbage",
+        "completed": false,
+    },
+    {
+        "id": 3,
+        "value": "walk the dog",
+        "completed": false,
+    },
+];
+
+const mockAction = {
+    payload: { 
+        id: 1,
+    },
+};
+
+describe("#1 fetchListSaga testing", () => {
+    function* mockSaga(api) {
+        const action = yield take(FETCH_LIST_REQUEST);
+        const response = yield call(api.getList, action.payload);
+
+        yield put({
+            type: FETCH_LIST_REQUEST,
+            payload: response,
+        });
+    };
+
+    test("#1 mockSaga", () => {
+        const api = {
+            getList: resource => ({ resource }),
+        };
+
+        return expectSaga(mockSaga, api)
+            .put({
+                type: FETCH_LIST_REQUEST,
+                payload: { resource: "mockResource" },
+            })
+            .dispatch({
+                type: FETCH_LIST_REQUEST,
+                payload: "mockResource",
+            })
+            .run();
+    });
+});
