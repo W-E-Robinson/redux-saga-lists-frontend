5f40d0e44000daec6c5c999f0d43fdfb0e361b1c
Will Robinson <william.ellis.robinson@gmail.com>
2023-01-10 19:55:13 +0000

    action test changes

--- src/modules/Redux/actions/lists/__tests__/index.ts
+++ src/modules/Redux/actions/lists/__tests__/index.ts
@@ -5,17 +5,12 @@ import {
 } from "../actions";
 
 describe("Actions functions testing", () => {
-    const functions = [
-        fetchListRequest,
-        fetchListSuccess,
-        fetchListFailure,
-    ];
-
-    for (const func of functions) {
-        test("function test", () => {
-            const f = func("");
-            expect(f.type).toBeTruthy();
-        });
-    }
+    test.each([
+        [fetchListRequest, "FETCH_LIST_REQUEST"],
+        [fetchListSuccess, "FETCH_LIST_SUCCESS"],
+        [fetchListFailure, "FETCH_LIST_FAILURE"],
+    ])("%s action should have type %s", (action, actionType: string) => {
+        expect(action().type).toBe(actionType);
+    });
 });
 
