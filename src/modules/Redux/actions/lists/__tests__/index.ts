7d6a93367b434468171849a767e5256cb42c90e7
Will Robinson <william.ellis.robinson@gmail.com>
2023-01-12 19:27:43 +0000

    actions tests added

--- src/modules/Redux/actions/lists/__tests__/index.ts
+++ src/modules/Redux/actions/lists/__tests__/index.ts
@@ -2,6 +2,9 @@ import {
     fetchListRequest,
     fetchListFailure,
     fetchListSuccess,
+    toggleCompletionRequest,
+    toggleCompletionSuccess,
+    toggleCompletionFailure,
 } from "../actions";
 
 describe("Actions functions testing", () => {
@@ -13,6 +16,15 @@ describe("Actions functions testing", () => {
     });
     test("#3 fetchListSuccess", () => {
         expect(fetchListSuccess({ list: [{ id: 1, value: "mock value", completed: true }] }).type).toBe("FETCH_LIST_SUCCESS");
+    });
+    test("#4 toggleCompletionRequest", () => {
+        expect(toggleCompletionRequest({ id: 1 }).type).toBe("TOGGLE_COMPLETION_REQUEST");
+    });
+    test("#5 toggleCompletionFailure", () => {
+        expect(toggleCompletionFailure({ error: "mock error" }).type).toBe("TOGGLE_COMPLETION_FAILURE");
+    });
+    test("#6 toggleCompletionSuccess", () => {
+        expect(toggleCompletionSuccess({ list: [{ id: 1, value: "mock value", completed: true }] }).type).toBe("TOGGLE_COMPLETION_SUCCESS");
     });
 });
 
